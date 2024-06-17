ad7bcdcc868bca067c5fc54e0cd5320892a40a78
Emerson Vieira <emersondont@gmail.com>
2024-06-17 20:29:15 -0300

    feat: Add expense card component and integrate with expenses page

--- src/app/(tabs)/expenses.tsx
+++ src/app/(tabs)/expenses.tsx
@@ -1,33 +1,95 @@
-import { StyleSheet, Text, View } from "react-native";
+import Layout from "../../components/layout";
+import { Text, Button } from "@ui-kitten/components";
+import ButtonSubmit, { Progress } from "../../components/forms/buttonSubmit";
+import { SubmitHandler, useForm } from "react-hook-form";
+import ExpenseForm from "../../components/forms/expense/expenseForm";
+import { useState } from "react";
+import { useRouter } from "expo-router";
+import { ExpenseSchema, ExpenseType, expenseSchema } from "../../types";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { ScrollView } from "react-native";
+import ExpenseCard from "../../components/expenseCard";
+import Animated, { FadeInDown } from "react-native-reanimated";
+import Header from "../../components/modal/header";
+import { useExpensesDatabase } from "../../database/useExpenseDatabase";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 
 export default function Expenses() {
+  const [progressStatus, setProgressStatus] = useState<Progress>('default')
+  const [newExpense, setNewExpense] = useState(false)
+  const { control, handleSubmit, setError, formState: { errors }, reset } = useForm<ExpenseSchema>({
+    resolver: zodResolver(expenseSchema),
+    defaultValues: {}
+  })
+  const expenseDatabase = useExpensesDatabase()
+  const { data: expenses } = useQuery({
+    queryKey: ["expenses"],
+    queryFn: () => expenseDatabase.getByMonth({ month: new Date().getMonth() + 1, year: new Date().getFullYear() }),
+  })
+  const queryClient = useQueryClient()
+  const { mutateAsync: createExpenseFn } = useMutation({
+    mutationFn: expenseDatabase.create,
+    onSuccess(data, variables) {
+      queryClient.setQueryData(["expenses"], (expenses: ExpenseType[]) => {
+        expenses = [...expenses, {
+          ...variables,
+          id: data.id
+        }]
+
+        expenses.sort((a, b) => {
+          const dateA = new Date(a.paymentDate);
+          const dateB = new Date(b.paymentDate);
+          return dateA.getTime() - dateB.getTime();
+        })
+
+        return expenses
+      })
+    }
+  })
+
+  const handleNewExpense: SubmitHandler<ExpenseSchema> = async (data) => {
+    try {
+      await createExpenseFn(data)
+      setProgressStatus('success')
+      setTimeout(() => {
+        setProgressStatus('default')
+        reset()
+      }, 400)
+    } catch (error) {
+      setError("root", { message: String(error) })
+    }
+  }
+  const onError = () => {
+    setProgressStatus('error')
+  }
+
   return (
-    <View style={styles.container}>
-      <View style={styles.main}>
-        <Text style={styles.title}>Gastos</Text>
-      </View>
-    </View>
+    <Layout>
+      <Text category="h4" style={{ marginBottom: 12 }}>Gastos do mês:</Text>
+      <ScrollView showsVerticalScrollIndicator={false} style={{ flex: 1, marginBottom: 12 }}>
+        {
+          expenses?.map(expense => (
+            <ExpenseCard key={expense.id} expense={expense} />
+          ))
+        }
+      </ScrollView>
+
+      {
+        newExpense ? (
+          <Animated.View entering={FadeInDown.duration(400)}>
+            <Header title="Adicionar:" handleClose={() => setNewExpense(false)} />
+            <ExpenseForm control={control} errors={errors} />
+
+            <ButtonSubmit
+              handleSubmit={handleSubmit(handleNewExpense, onError)}
+              errors={errors}
+              progressStatus={progressStatus}
+              setProgressStatus={setProgressStatus}
+            />
+          </Animated.View>
+        ) :
+          <Button onPress={() => setNewExpense(true)}>Adicionar</Button>
+      }
+    </Layout>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    padding: 24,
-  },
-  main: {
-    flex: 1,
-    justifyContent: "center",
-    maxWidth: 960,
-    marginHorizontal: "auto",
-  },
-  title: {
-    fontSize: 64,
-    fontWeight: "bold",
-  },
-  subtitle: {
-    fontSize: 36,
-    color: "#38434D",
-  },
-});
---
+++ src/components/expenseCard.tsx
@@ -0,0 +1,38 @@
+import { Card, Text } from "@ui-kitten/components";
+import { View } from "react-native";
+import Animated, { FadeIn } from "react-native-reanimated";
+import { ExpenseType } from "../types";
+
+interface Props {
+  expense: ExpenseType
+}
+
+export default function ExpenseCard({ expense }: Props) {
+  const date = new Date(expense.paymentDate)
+  return (
+    <Animated.View entering={FadeIn.duration(200)}>
+      <Card
+        style={{ marginBottom: 6 }}
+      // onPress={() => setVisible(true)}
+      >
+        <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
+          <View style={{ flexDirection: 'row', gap: 6, alignItems: 'center', width: '70%' }}>
+            <Text category='label' appearance="hint">
+              {date.toISOString().slice(8, 10)}/{date.toISOString().slice(5, 7)}
+            </Text>
+            <Text category='p1' numberOfLines={1} ellipsizeMode="tail" style={{ maxWidth: '50%' }}>
+              {expense.product}
+            </Text>
+            <Text category='p1' numberOfLines={1} ellipsizeMode="tail" style={{ maxWidth: '30%' }}>
+              {expense.quantity}
+            </Text>
+          </View>
+          <Text category='h6' status="danger">
+            {expense.price.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })}
+          </Text>
+        </View>
+      </Card>
+
+    </Animated.View>
+  )
+}
